9bc40640900cce4e700bbcead814179dab5aee47
Vitalii Drahun <vdrag00n90@gmail.com>
2021-02-10 15:04:24 +0200

    Fix integration tests

--- licensing-service/src/test/java/com/optimagrowth/license/LicenseServiceApplicationTests.java
+++ licensing-service/src/test/java/com/optimagrowth/license/LicenseServiceApplicationTests.java
@@ -2,8 +2,13 @@ package com.optimagrowth.license;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.stream.binder.test.TestChannelBinderConfiguration;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
+@Import(TestChannelBinderConfiguration.class)
 class LicenseServiceApplicationTests {
 
     @Test
